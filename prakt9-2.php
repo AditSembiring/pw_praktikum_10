e8fd0983c9c5958c8d5cc01b95defce977662e72
AditSembiring <adit3gmg@gmail,com>
2024-06-13 17:00:57 +0700

    pertemuan 10 dan challenge

---
+++ prakt9-2.php
@@ -0,0 +1,9 @@
+<?php
+$nama = $_POST['namaku'];
+$email = $_POST['emailku'];
+$alamat = $_POST['alamatku'];
+echo "<h2> Data diri saya</h2>";
+echo "Nama : <b>$nama</b> <br>";
+echo "E-mail : <b>$email</b> <br>";
+echo "Pesan : <b>$alamat</b> <br>";
+?>
